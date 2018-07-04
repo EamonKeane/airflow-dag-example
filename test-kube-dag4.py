6191bd274db76132a9860d17157ca9a1399e2665
Eamon Keane <eamon.keane1@gmail.com>
2018-07-04 15:40:51 +0100

    Update test-kube-dag4.py

--- test-kube-dag4.py
+++ test-kube-dag4.py
@@ -16,7 +16,7 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_sample6', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'kubernetes_sample7', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
